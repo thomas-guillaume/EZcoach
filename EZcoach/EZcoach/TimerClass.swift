94f9998b9ae0cdef150e25f234c8399a642894ed
thomas-guillaume <thomaseq.guillaume@gmail.com>
2018-12-01 00:14:12 +0800

    stack views op

--- EZcoach/EZcoach/TimerClass.swift
+++ EZcoach/EZcoach/TimerClass.swift
@@ -13,69 +13,45 @@ class TimerClass: UIViewController {
     
     @IBOutlet weak var timerLbl: UILabel!
     
-    
-    var serieNb:Int!
-    var seconds = 3
-    var isTimerRunning = false
     var timer = Timer()
-    
-    
-    
-    
-    
-    @objc func fireTimer() {
-        if(seconds > 0)
-        {
-            seconds -= 1     //This will decrement(count down)the seconds.
-            timerLbl.text = "\(seconds)" //This will update the label.
-        }
-        else
-        {
-            if(serieNb < 4)
-            {
-                
-                serieNb += 1
-                UserDefaults.standard.set(serieNb, forKey: "serie")
-                goTo(view: "serie"+String(serieNb))
-            }
-            else
-            {
-                serieNb = 1
-                UserDefaults.standard.set(serieNb, forKey: "serie")
-            }
-        }
-    }
-    
-    
-    func goTo(view : String)
-    {
-        let main = UIStoryboard(name: "Main", bundle: nil    )
-        let second = main.instantiateViewController(withIdentifier: view)
-        
-        self.present(second,animated:true,completion:nil)
-    }
+    var time = 2
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let nb: Int = UserDefaults.standard.object(forKey : "serie") as? Int   {
-            serieNb = nb
-        } else {
-            serieNb = 1
-        }
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
         
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.darkGray
+        
+        
+        
+        // Start the timer
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    
+    @objc func fireTimer() {
+        if(time > 0)
+        {
+            //This will decrement(count down)the seconds.
+            time -= 1
+            
+            //This will update the label.
+            if time < 10 {
+                timerLbl.text = "00:0\(time)"
+            } else {
+                timerLbl.text = "00:\(time)"
+            }
+        } else {
+            goTo(view: "serie2")
+        }
+    }
+    
+    func goTo(view : String)
+    {
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let second = main.instantiateViewController(withIdentifier: view)
+        
+        self.present(second,animated:true,completion:nil)
+    }
 }
