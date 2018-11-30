8a12fd4467e26ec6a4d59283391017aa4cd99ca4
Mihawk93 <math.pillot@gmail.com>
2018-11-29 18:58:35 -0800

    improve the timer feature

--- EZcoach/EZcoach/TimerClass.swift
+++ EZcoach/EZcoach/TimerClass.swift
@@ -13,36 +13,69 @@ class TimerClass: UIViewController {
     
     @IBOutlet weak var timerLbl: UILabel!
     
-    var seconds = 60
+    
+    var serieNb:Int!
+    var seconds = 3
     var isTimerRunning = false
     var timer = Timer()
     
     
     
+    
+    
     @objc func fireTimer() {
-        print("Timer fired!")
-        seconds -= 1     //This will decrement(count down)the seconds.
-        timerLbl.text = "\(seconds)" //This will update the label.
+        if(seconds > 0)
+        {
+            seconds -= 1     //This will decrement(count down)the seconds.
+            timerLbl.text = "\(seconds)" //This will update the label.
+        }
+        else
+        {
+            if(serieNb < 4)
+            {
+                
+                serieNb += 1
+                UserDefaults.standard.set(serieNb, forKey: "serie")
+                goTo(view: "serie"+String(serieNb))
+            }
+            else
+            {
+                serieNb = 1
+                UserDefaults.standard.set(serieNb, forKey: "serie")
+            }
+        }
     }
     
+    
+    func goTo(view : String)
+    {
+        let main = UIStoryboard(name: "Main", bundle: nil    )
+        let second = main.instantiateViewController(withIdentifier: view)
+        
+        self.present(second,animated:true,completion:nil)
+    }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let nb: Int = UserDefaults.standard.object(forKey : "serie") as? Int   {
+            serieNb = nb
+        } else {
+            serieNb = 1
+        }
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
         
         // Do any additional setup after loading the view.
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
